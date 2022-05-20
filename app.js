23bc006c2c445c3ac999d2c4c3cd6494bcdb14be
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2022-05-20 20:55:40 +0800

    3 Fundamental Exercise

---
+++ app.js
@@ -0,0 +1,62 @@
+'use strict';
+//JS FUNDAMENTALS
+
+//1.
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+// function maps(x) {
+// 	return x.map((val) => val * 2);
+// }
+// console.log(maps([ 1, 2, 3 ]));
+
+//2.
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Sum Numbers
+// function sum(numbers) {
+// 	let a = numbers.reduce((acc, val) => {
+// 		return (acc += val);
+// 	}, 0);
+// 	return a;
+// }
+// console.log(sum([ 1, 5.2, 4, 0, -1 ]));
+
+//3.
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+// function betterThanAverage(classPoints, yourPoints) {
+// 	// Your code here
+// 	let a = 0;
+// 	for (let i = 0; i < classPoints.length; i++) {
+// 		a += classPoints[i];
+// 	}
+// 	if (yourPoints > a / classPoints.length) {
+// 		return true;
+// 	}
+// 	return false;
+
+//     //Alternative
+//     // return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+// }
+
+// console.log(betterThanAverage([ 4, 3 ], 5));
