95bbef530da289c1fbe7714f90c564459deacf60
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2022-05-24 01:16:12 +0800

    Update app.js
    No. 14

--- app.js
+++ app.js
@@ -411,3 +411,24 @@
 // 	return b;
 // }
 // console.log(removeEveryOther([ 'Hello', 'Goodbye', 'Hello Again' ]));
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//14.Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+// Examples:
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+//SOLUTION
+// var number = function(array) {
+// 	//your awesome code here
+// 	if (array.length < 1) return [];
+// 	let a = [];
+// 	array.forEach((element, index) => {
+// 		a.push(`${index + 1}: ${element}`);
+// 	});
+// 	return a;
+// };
+// console.log(number([ 'a', 'b', 'c' ]));
