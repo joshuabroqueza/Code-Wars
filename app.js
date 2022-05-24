0aec109ff58ad5984975bf918e3b1b6fc138fdd5
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2022-05-25 00:54:40 +0800

    Update app.js
    17

--- app.js
+++ app.js
@@ -449,33 +449,44 @@
 
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
-//16. Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+// //16. Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
 
-// For example: ["3:1", "2:2", "0:1", ...]
+// // For example: ["3:1", "2:2", "0:1", ...]
 
-// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+// // Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
 
-// if x > y: 3 points
-// if x < y: 0 point
-// if x = y: 1 point
-// Notes:
+// // if x > y: 3 points
+// // if x < y: 0 point
+// // if x = y: 1 point
+// // Notes:
 
-// there are 10 matches in the championship
-// 0 <= x <= 4
-// 0 <= y <= 4
+// // there are 10 matches in the championship
+// // 0 <= x <= 4
+// // 0 <= y <= 4
 
-//SOLUTION
-function points(games) {
-    // your code here
-    let score = 0;
-    games.forEach(element => {
-        console.log(element[0]);
-        if (element[0] > element[2]) {
-            score += 3;
-        } else if (element[0] === element[2]) {
-            score += 1;
-        }
-    });
-    return score;
-}
-console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"]));
+// //SOLUTION
+// function points(games) {
+//     // your code here
+//     let score = 0;
+//     games.forEach(element => {
+//         console.log(element[0]);
+//         if (element[0] > element[2]) {
+//             score += 3;
+//         } else if (element[0] === element[2]) {
+//             score += 1;
+//         }
+//     });
+//     return score;
+// }
+// console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"]));
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+//17. Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// function solution(str) {
+//     return str.split("").reverse().join("");
+// }
+// console.log(solution('world'));
