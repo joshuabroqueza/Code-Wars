95e2d120304bd19fee954b8877456a6f5180ecbb
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2022-05-25 20:10:47 +0800

    Update app.js
    18, 19

--- app.js
+++ app.js
@@ -490,3 +490,50 @@
 //     return str.split("").reverse().join("");
 // }
 // console.log(solution('world'));
+
+///////////////////////////////////////
+///////////////////////////////////////
+//18. Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true, true, true, false,
+//     true, true, true, true,
+//     true, false, true, false,
+//     true, false, false, true,
+//     true, true, true, true,
+//     false, false, true, true
+// ]
+// The correct answer would be 17.
+//
+//SOLUTION
+// function countSheeps(arrayOfSheep) {
+//     // TODO May the force be with you
+//     let counter = 0;
+//     arrayOfSheep.forEach(element => {
+//         if (element === true) {
+//             counter++;
+//         }
+//     });
+//     return counter;
+// }
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+//19. This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+//SOLUTION
+function simpleMultiplication(number) {
+    // your code........
+    if (number % 2 === 0) {
+        return number * 8;
+    }
+    return number * 9;
+}
+
+//ALTERNATIVE
+function simpleMultiplicatiozn(number) {
+    return number % 2 === 0 ? number * 8 : number * 9;
+}
+console.log(simpleMultiplication(4));
+console.log(simpleMultiplicatiozn(4));
