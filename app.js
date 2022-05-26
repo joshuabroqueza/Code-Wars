cefbcec31791f458e9dcf3d471c01502166b6340
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2022-05-27 04:42:56 +0800

    Update app.js
    22, 23, 24

--- app.js
+++ app.js
@@ -583,7 +583,7 @@
 //     // ...
 //     let sums = [];
 
-//     for (let i = 0; i < numbers.length; i++) {
+//     for (let i = 0; i < numbers.length ; i++) {
 //         for (let j = i + 1; j < numbers.length; j++) {
 //             if (numbers[i] + numbers[j] === target) {
 //                 console.log(numbers[i], i);
@@ -598,4 +598,69 @@
 // console.log(twoSum([1, 2, 3], 4));
 
 /////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
+/////////////// ////////////////////////////////////
+//. 22 You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+// //SOLUTION
+// function check(a, x) {
+//     let b = false;
+//     a.forEach(element => {
+//         if (element === x) {
+//             b = true;
+//         };
+
+//     });
+//     return b;
+// }
+
+// //ALTERNATIVE
+// const check = (a,x) => a.includes(x);
+
+/////////////////////////////////////////////////////
+/////////////// ////////////////////////////////////
+
+//23. You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+// Write a program that returns the girl's age (0-9) as an integer.
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+// //SOLUTION
+// function getAge(inputString) {
+//     // return the girl's correct age as an integer. Happy coding :) 
+//     return Number(inputString[0])
+// }
+// console.log(getAge("4 years old"));
+/////////////////////////////////////////////////////
+/////////////// ////////////////////////////////////
+
+// //24. Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+// // Examples
+// // "the-stealth-warrior" gets converted to "theStealthWarrior"
+// // "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+
+// function toCamelCase(str) {
+//     str = str.split('');
+//     return str.map(function (el, i) {
+//         if (el == '-' || el == '_') {
+//             el = str[i + 1].toUpperCase();
+//             str.splice(i + 1, 1);
+//         }
+//         return el;
+//     }).join('');
+
+// }
+
+// console.log(toCamelCase("the_stealth_warrior"));
+
+// //ALTERNATE
+// function toCamelCase(str){
+//     var regExp=/[-_]\w/ig;
+//     return str.replace(regExp,function(match){
+//           return match.charAt(1).toUpperCase();
+//      });
+// }
+// //////////////////////////////////////////
+// ///////////////////////////////////////////
