d2f0cf60b88b7c486a107a208d1a28e9a2f15a78
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2022-07-11 09:47:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1882,7 +1882,7 @@ Given an array of strings, return an array where
 the first letter of each string is capitalized
 */
 
-const names = ['alice', 'bob', 'charlie', 'danielle'];
+const names = ["alice", "bob", "charlie", "danielle"];
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
 // Your code here
 const capitalized = names.map((name) => name[0].toUpperCase() + name.slice(1));
@@ -1898,7 +1898,7 @@ E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
 return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 */
 
-const pokemon = ['Bulbasaur', 'Charmander', 'Squirtle'];
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
 // -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 // Your code here
 
@@ -1972,3 +1972,36 @@ function pipeFix(numbers) {
   }
   return a;
 }
+
+///EXERCISE
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+// A few examples:(Input1, Input2, Input3 --> Output)
+
+// 5, 2, "add"      --> 7
+// 5, 2, "subtract" --> 3
+// 5, 2, "multiply" --> 10
+// 5, 2, "divide"   --> 2.5
+
+//SOLUTION
+function arithmetic(a, b, operator) {
+  //your code here!
+  if (operator == "add") {
+    return a + b;
+  } else if (operator == "subtract") {
+    return a - b;
+  } else if (operator == "multiply") {
+    return a * b;
+  } else if (operator == "divide") {
+    return a / b;
+  }
+}
+
+console.log(arithmetic(1, 2, "add"));
+console.log(arithmetic(8, 2, "subtract"));
+console.log(arithmetic(5, 2, "multiply"));
+console.log(arithmetic(8, 2, "divide"));
